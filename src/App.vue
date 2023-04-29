9134d79a2063b90fa357cd0d6404a299e7039c48
baken667 <baken.wws@gmail.com>
2023-04-29 09:44:06 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import MarqueeGsap from "./components/MarqueeGsap.vue";
+</script>
+
+<template>
+  <div class="main">
+    <div class="content">
+      <MarqueeGsap />
+    </div>
+  </div>
+</template>
